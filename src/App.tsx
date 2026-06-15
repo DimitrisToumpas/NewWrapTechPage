93670db94131afe124b50f567368f9dad064c376
DimitrisToumpas <115160521+DimitrisToumpas@users.noreply.github.com>
2026-06-15 07:04:34 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import WrapTechRed from './WrapTechRed';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ServiceDetailsPage from './ServiceDetailsPage';
+import Main from './WrapTechRed';
 
-function App() {
-  return <WrapTechRed />;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        {/* Δυναμικό url που δέχεται το όνομα της υπηρεσίας */}
+        <Route path="/services/:serviceId" element={<ServiceDetailsPage />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
